e4cb3eb0bbadd025618e5d7009e5b710fa1f9176
AZ <wira123widodo@gmail.com>
2023-05-01 21:20:58 +0700

    style[AW]: perbaiki struktur pada beranda admin

--- src/admin/BerandaAdmin.jsx
+++ src/admin/BerandaAdmin.jsx
@@ -22,9 +22,9 @@ function showDetail() {
   }
 
   return(
-      <div>
+      <div className='body'>
           {/* <Sidebar /> */}
-          <aside>
+        <aside>
             <h1 className="title-form-login" onClick={() => navigate('/berandaadmin')}>
                 <img src={IconNugasyuk} alt="" className="icon-nugasyuk"/>
                 nugasyuk
@@ -59,174 +59,96 @@ function showDetail() {
                     Assets
                 </li>
             </ul>
-            </aside>
-          <div className="container-content">
-              <Navigation text={navText}/>
-              <main className='main'>
+        </aside>
+
+        <div className="container-content">
+            <Navigation text={navText}/>
+            <main className='main'>
                 <div className="header-dashboard">
                     <div className="head-left">
-                    <h1 className="intro-head">
-                        Halo <span className="name-admin">Erika Yanti, S.pd</span>
-                    </h1>
-                    <p className="desc-head">
-                        Selamat datang di admin nugasyuk, anda bisa memonitoring data guru, siswa dan lain lain.
-                    </p>
+                        <h1 className="intro-head">Halo <span className="name-admin">Erika Yanti, S.pd</span></h1>
+                        <p className="desc-head">Selamat datang di admin nugasyuk, anda bisa memonitoring data guru, siswa dan lain lain.</p>
                     </div>
                     <div className="head-right">
-                    <div className="kotak1"></div>
+                        <div className="kotak1"></div>
                     </div>
                 </div>
 
-                <div className="con-content">
+                <div className="con-content">   
                     <div className="content-indiecator" style={{ background: "#EB55A3" }}>
-                    <div className="icon-indie" style={{ color: "#EB55A3" }}>
-                        <Icon icon="mdi:account-group-outline" width="40" />
+                        <div className="icon-indie" style={{ color: "#EB55A3" }}>
+                            <Icon icon="mdi:account-group-outline" width="40" />
+                        </div>
+                        <div className="desc-indie">
+                            <p className="title-indie">Jumlah Siswa Keseluruhan</p>
+                            <p className="value-indie"><span>1000</span> Siswa</p>
+                        </div>
                     </div>
-                    <div className="desc-indie">
-                        <p className="title-indie">Jumlah Siswa Keseluruhan</p>
-                        <p className="value-indie">
-                        <span>1000</span> Siswa
-                        </p>
-                    </div>
-                    </div>
+
                     <div className="content-indiecator" style={{ background: "#2A93D5" }}>
-                    <div className="icon-indie" style={{ color: "#2A93D5" }}>
-                        <Icon icon="fluent:class-24-regular" width="40" />
+                        <div className="icon-indie" style={{ color: "#2A93D5" }}>
+                            <Icon icon="fluent:class-24-regular" width="40" />
+                        </div>
+                        <div className="desc-indie">
+                            <p className="title-indie">Jumlah Kelas Keseluruhan</p>
+                            <p className="value-indie">
+                            <span>25</span> Kelas</p>
+                        </div>
                     </div>
-                    <div className="desc-indie">
-                        <p className="title-indie">Jumlah Kelas Keseluruhan</p>
-                        <p className="value-indie">
-                        <span>25</span> Kelas
-                        </p>
-                    </div>
-                    </div>
+
                     <div className="content-indiecator" style={{ background: "#B462D0" }}>
-                    <div className="icon-indie" style={{ color: "#B462D0" }}>
-                        <Icon icon="la:chalkboard-teacher" width="40" />
+                        <div className="icon-indie" style={{ color: "#B462D0" }}>
+                            <Icon icon="la:chalkboard-teacher" width="40" />
+                        </div>
+                        <div className="desc-indie">
+                            <p className="title-indie">Jumlah Guru Keseluruhan</p>
+                            <p className="value-indie"><span>30</span> Guru</p>
+                        </div>
                     </div>
-                    <div className="desc-indie">
-                        <p className="title-indie">Jumlah Guru Keseluruhan</p>
-                        <p className="value-indie">
-                        <span>30</span> Guru
-                        </p>
-                    </div>
-                    </div>
+
                     <div className="content-indiecator" style={{ background: "#585CC4" }}>
-                    <div className="icon-indie" style={{ color: "#585CC4" }}>
-                        <Icon icon="fluent-mdl2:education" width="40" />
-                    </div>
-                    <div className="desc-indie">
-                        <p className="title-indie">Jumlah Jurusan</p>
-                        <p className="value-indie">
-                        <span>5</span> Jurusan
-                        </p>
-                    </div>
+                        <div className="icon-indie" style={{ color: "#585CC4" }}>
+                            <Icon icon="fluent-mdl2:education" width="40" />
+                        </div>
+                        <div className="desc-indie">
+                            <p className="title-indie">Jumlah Jurusan</p>
+                            <p className="value-indie">
+                            <span>5</span> Jurusan</p>
+                        </div>
                     </div>
                 </div>
             </main>
-          </div>
-      </div>
+        </div> {/*  end container */}
+
+        <div className="detail-profile">
+            <div>
+                <div className="navbar-detail">
+                <Icon icon="radix-icons:cross-circled" width="30" style={{cursor: "pointer"}} />
+                <h2>Profil</h2>
+                </div>
+                <div className="detail-image-profile">
+                    <img src={ImgProfil} alt="" className="detail-img-profile" />
+                </div>
+                <p className="judul-detail">Email</p>
+                <p className="value-detail">erikayanti@smkrus.sch.id</p>
+                <p className="judul-detail">Nama</p>
+                <p className="value-detail">Erika Yanti, S.Pd</p>
+                <p className="judul-detail">Devisi</p>
+                <p className="value-detail">Admin</p>
+            </div>
+            <div className="con-btn-detail-profile">
+                <button className="forget-password" id="btn-forget-pass">
+                <Icon icon="material-symbols:key-outline-rounded" width="30" />
+                <p>Ganti Password</p>
+                </button>
+                <button className="logout" id="btn-logout">
+                <Icon icon="material-symbols:logout-rounded" width="30" />
+                <p>Logout</p>
+                </button>
+            </div>
+        </div>
+    </div> /*body*/
   );
 }
-
-// function PopupForget() {
-//     const [passwordShown, setPasswordShown] = useState(false);
-//     const [newPasswordShown, setNewPasswordShown] = useState(false);
-//     const [confirmPasswordShown, setConfirmPasswordShown] = useState(false);
-  
-//     const togglePassword = () => {
-//       setPasswordShown(!passwordShown);
-//     };
-  
-//     const togglePasswordNew = () => {
-//       setNewPasswordShown(!newPasswordShown);
-//     };
-  
-//     const togglePasswordConfirm = () => {
-//       setConfirmPasswordShown(!confirmPasswordShown);
-//     };
-  
-//     const closeForgetPopup = () => {
-//       // fungsi untuk menutup popup
-//     };
-  
-//     const handleSubmit = (e) => {
-//       e.preventDefault();
-//       // fungsi untuk mengirim data form
-//     };
-  
-//     return (
-//       <div className="popup-forget" id="popup-forget">
-//         <form onSubmit={handleSubmit} className="detail-forget-password">
-//           <div className="navbar-detail-forget">
-//             <Icon
-//               icon="radix-icons:cross-circled"
-//               onclick="closeLogoutPopup()"
-//               width="30"
-//               onClick={closeForgetPopup}
-//               style={{ cursor: "pointer" }}
-//             ></Icon>
-//             <h2>Ganti Password</h2>
-//           </div>
-//           <p className="judul-form">Sandi lama</p>
-//           <div className="con-form-password">
-//             <img src={passIcon} alt="" />
-//             <input
-//               type={passwordShown ? "text" : "password"}
-//               id="password"
-//               placeholder="*********"
-//               className="input-password"
-//             />
-//             <button
-//               type="button"
-//               onClick={togglePassword}
-//               className="btn-mata"
-//             >
-//               <img src={mataIcon} alt="" />
-//             </button>
-//           </div>
-//           <p className="judul-form">Sandi baru</p>
-//           <div className="con-form-password">
-//             <img src={passIcon} alt="" />
-//             <input
-//               type={newPasswordShown ? "text" : "password"}
-//               id="newPassword"
-//               placeholder="*********"
-//               className="input-password"
-//             />
-//             <button
-//               type="button"
-//               onClick={togglePasswordNew}
-//               className="btn-mata"
-//             >
-//               <img src={mataIcon} alt="" />
-//             </button>
-//           </div>
-//           <p className="judul-form">Konfirmasi sandi baru</p>
-//           <div className="con-form-password">
-//             <img src={passIcon} alt="" />
-//             <input
-//               type={confirmPasswordShown ? "text" : "password"}
-//               id="confirmPassword"
-//               placeholder="*********"
-//               className="input-password"
-//             />
-//             <button
-//               type="button"
-//               onClick={togglePasswordConfirm}
-//               className="btn-mata"
-//             >
-//               <img src={mataIcon} alt="" />
-//             </button>
-//           </div>
-  
-//           <button type="submit" className="btn-simpan">
-//             Simpan sandi baru
-//           </button>
-//         </form>
-//       </div>
-//     );
-//   }
-
 
 export default BerandaAdmin;
--- src/component/NavigationBar.jsx
+++ src/component/NavigationBar.jsx
@@ -6,60 +6,16 @@ import React, { useState } from 'react';
 
 function Navigation(props) {
 
-  const [show, setShow] = useState(false);
-
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-
-  const showDetail = () => {
-    // alert("Halloooo, kali ini saya sedang ngetess");
-    // document.querySelector('');
-    // detailProfile.style.transform = 'translateX(0px)';
-  }
-
-  function closeDetail() {
-    // document.querySelector('.detail-profile');
-    // detailProfile.style.transform = 'translateX(100%)';
-  }
-
-
   return (
     <div>
       <nav>
         <div className="navbar">
           <h1>{props.text}</h1>
           <div className="img-profile" style={{ cursor: "pointer" }}>
-            <img src={ImgProfil} alt="img-profile" onClick={handleShow} />
+            <img src={ImgProfil} alt="img-profile"/>
           </div>
         </div>
       </nav>
-      <div className="detail-profile" show={show} onHide={handleClose}>
-        <div>
-            <div className="navbar-detail">
-            <Icon icon="radix-icons:cross-circled" width="30" style={{cursor: "pointer"}} />
-            <h2>Profil</h2>
-            </div>
-            <div className="detail-image-profile">
-                <img src={ImgProfil} alt="" className="detail-img-profile" />
-            </div>
-            <p className="judul-detail">Email</p>
-            <p className="value-detail">erikayanti@smkrus.sch.id</p>
-            <p className="judul-detail">Nama</p>
-            <p className="value-detail">Erika Yanti, S.Pd</p>
-            <p className="judul-detail">Devisi</p>
-            <p className="value-detail">Admin</p>
-        </div>
-        <div className="con-btn-detail-profile">
-            <button className="forget-password" id="btn-forget-pass">
-            <Icon icon="material-symbols:key-outline-rounded" width="30" />
-            <p>Ganti Password</p>
-            </button>
-            <button className="logout" id="btn-logout">
-            <Icon icon="material-symbols:logout-rounded" width="30" />
-            <p>Logout</p>
-            </button>
-        </div>
-    </div>
     </div>
   );
 }
