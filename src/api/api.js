3800233f18c1bf6ecb2bbc764a0e6c92b9655811
AZ <wira123widodo@gmail.com>
2023-08-02 21:05:24 +0700

    style[AW]: change link API

--- src/api/api.js
+++ src/api/api.js
@@ -1,4 +1,4 @@
 // api/apiurl.js
-const apiurl = "https://d4b7-103-164-229-68.ngrok-free.app/api/";
+const apiurl = "https://wondrous-squirrel-blatantly.ngrok-free.app/api/";
 
 export default apiurl;
