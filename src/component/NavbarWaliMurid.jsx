8f489116ce1232815419793ce8da03fe27df1ac2
zumarRamadhan <zumar.ramadhan46@gmail.com>
2023-08-07 08:24:57 +0700

    fix [ZMR] : fixing profile ortu in branch role/walimurid

--- src/component/NavbarWaliMurid.jsx
+++ src/component/NavbarWaliMurid.jsx
@@ -1,70 +1,288 @@
-import axios from 'axios';
-import '../App.css';
-import ImgProfil from '../assets/profil-walimurid.svg';
-import { Icon } from '@iconify/react';
-import React, { useState, useEffect } from 'react';
-import apiurl from '../api/api';
+import axios from "axios";
+import "../App.css";
+import ImgProfil from "../assets/profil-walimurid.svg";
+import IconNugasyuk from "../assets/IconNugasyuk.svg";
+import NavbarMurid from "../component/NavbarMurid";
+import ImgLogout from "../assets/68582-log-out.gif";
+import passIcon from "../assets/pass-icon.svg";
+import mataIcon from "../assets/icon-mata.svg";
+import { Icon } from "@iconify/react";
+import React, { useState, useEffect } from "react";
+import apiurl from "../api/api";
 
+function NavbarWaliMurid(props) {
+  const showDetail = () => {
+    const detailProfile = document.querySelector(".detail-profile");
+    detailProfile.style.transform = "translateX(0px)";
+  };
 
-function NavbarWaliMurid(props){
+  const closeDetail = () => {
+    const detailProfile = document.querySelector(".detail-profile");
+    detailProfile.style.transform = "translateX(350px)";
+  };
 
-    const showDetail = () => {
-        const detailProfile = document.querySelector('.detail-profile');
-        detailProfile.style.transform = 'translateX(0px)';
-    }
+  const showNotif = () => {
+    const detailNotification = document.querySelector(".detail-notif");
+    detailNotification.style.transform = "translateX(0px)";
+  };
 
-    const showNotif = () => {
-        const detailNotification = document.querySelector('.detail-notif');
-        detailNotification.style.transform = 'translateX(0px)';
-    }
+  const showLogoutPopup = () => {
+    const popupLogout = document.querySelector("#popup-logout");
+    popupLogout.style.display = "flex";
+    popupLogout.style.animation = "slide-down 0.3s ease-in-out";
+  };
 
-    const {navigasiOrtu} = props;
+  const closeLogoutPopup = () => {
+    const popupLogout = document.querySelector("#popup-logout");
+    setTimeout(() => (popupLogout.style.display = "none"), 250);
+    popupLogout.style.animation = "slide-up 0.3s ease-in-out";
+  };
 
-    const saveToken = sessionStorage.getItem("token");
+  const showForgetPopup = () => {
+    const popupForget = document.querySelector("#popup-forget");
+    popupForget.style.display = "flex";
+    popupForget.style.animation = "slide-down 0.3s ease-in-out";
+  };
 
-    const [dataProfileOrtu, setDataProfileOrtu] = useState([]);
-    const [isError, setisError] = useState(false);
-  
-    useEffect(() => {
-      axios
-        .get(`${apiurl}ortu/profile`, {
-          headers: {
-            "Content-Type": "application/json",
-            "ngrok-skip-browser-warning": "any",
-            Authorization: `Bearer ${saveToken}`,
-          },
-        })
-        .then((result) => {
-          console.log("data API", result.data);
-          const responseAPI = result.data;
-  
-          setDataProfileOrtu(responseAPI.data);
-        })
-        .catch((err) => {
-          console.log("terjadi kesalahan: ", err);
-          setisError(true);
-        });
-    }, []);
-
-    return(
-        <div>
-            <nav>
-                <div className="navbar">
-                    <h1>{navigasiOrtu}</h1>
-                    <div className="nav-right">
-                        {dataProfileOrtu.map((ortuProfile) =>
-                            <div className="img-profile" style={{ cursor: "pointer" }}>
-                                <img src={`https://www.nugasyuk.my.id/public/${ortuProfile.foto_profile}`}alt="img-profile" onClick={showDetail} />
-                            </div>
-                        )}
-                        <div className="btn-notification" style={{ cursor: "pointer" }} onClick={showNotif}>
-                            <Icon icon="mdi:bell-notification-outline" width="24"/>
-                        </div>
-                    </div>
-                </div>
-            </nav>
-        </div>
+  const closeForgetPopupAndClearInput = () => {
+    const popupForget = document.querySelector("#popup-forget");
+    setTimeout(() => (popupForget.style.display = "none"), 250);
+    popupForget.style.animation = "slide-up 0.3s ease-in-out";
+    const clearpassword = document.querySelector(
+      "#password",
+      "#newPassword",
+      "#confirmPassword"
     );
+    clearpassword.value = "";
+    const clearpasswordNew = document.querySelector("#newPassword");
+    clearpasswordNew.value = "";
+    const clearpasswordConfirm = document.querySelector("#confirmPassword");
+    clearpasswordConfirm.value = "";
+  };
+
+  const [passwordType, setPasswordType] = useState("password");
+  const [passwordTypeNew, setPasswordTypeNew] = useState("password");
+  const [passwordTypeConfirm, setPasswordTypeConfirm] = useState("password");
+
+  function togglePasswordVisibility() {
+    setPasswordType(passwordType === "password" ? "text" : "password");
+  }
+
+  function togglePasswordVisibilityNew() {
+    setPasswordTypeNew(passwordTypeNew === "password" ? "text" : "password");
+  }
+
+  function togglePasswordVisibilityConfirm() {
+    setPasswordTypeConfirm(
+      passwordTypeConfirm === "password" ? "text" : "password"
+    );
+  }
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    window.location.href = "/login";
+  };
+
+  const { navigasiOrtu } = props;
+
+  const saveToken = sessionStorage.getItem("token");
+
+  const [dataProfileOrtu, setDataProfileOrtu] = useState([]);
+  const [isError, setisError] = useState(false);
+
+  useEffect(() => {
+    axios
+      .get(`${apiurl}ortu/profile`, {
+        headers: {
+          "Content-Type": "application/json",
+          "ngrok-skip-browser-warning": "any",
+          Authorization: `Bearer ${saveToken}`,
+        },
+      })
+      .then((result) => {
+        console.log("data API", result.data);
+        const responseAPI = result.data;
+
+        setDataProfileOrtu(responseAPI.data);
+      })
+      .catch((err) => {
+        console.log("terjadi kesalahan: ", err);
+        setisError(true);
+      });
+  }, []);
+
+  return (
+    <div>
+      <nav>
+        <div className="navbar">
+          <h1>{navigasiOrtu}</h1>
+          <div className="nav-right">
+            {dataProfileOrtu.map((ortuProfile) => (
+              <div className="img-profile" style={{ cursor: "pointer" }}>
+                <img
+                  src={`https://www.nugasyuk.my.id/public/${ortuProfile.foto_profile}`}
+                  alt="img-profile"
+                  onClick={showDetail}
+                />
+              </div>
+            ))}
+            <div
+              className="btn-notification"
+              style={{ cursor: "pointer" }}
+              onClick={showNotif}
+            >
+              <Icon icon="mdi:bell-notification-outline" width="24" />
+            </div>
+          </div>
+        </div>
+      </nav>
+
+      <div className="popup-logout" id="popup-logout">
+        <div className="detail-logout">
+          <Icon
+            icon="radix-icons:cross-circled"
+            width="30"
+            style={{ cursor: "pointer" }}
+            onClick={closeLogoutPopup}
+          />
+          <div className="image-logout">
+            <img src={ImgLogout} alt="" className="img-logout" />
+          </div>
+          <p className="desc-logout">Anda yakin ingin keluar?</p>
+          <div className="con-btn-logout">
+            <button type="button" className="btn-batal">
+              Batal
+            </button>
+            <button type="button" className="btn-keluar" onClick={logout}>
+              Keluar
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div className="popup-forget" id="popup-forget">
+        <form action="" className="detail-forget-password">
+          <div className="navbar-detail-forget">
+            <Icon
+              icon="radix-icons:cross-circled"
+              width="30"
+              style={{ cursor: "pointer" }}
+              onClick={closeForgetPopupAndClearInput}
+            />
+            <h2>Ganti Password</h2>
+          </div>
+          <p className="judul-form">Sandi lama</p>
+          <div className="con-form-password">
+            <img src={passIcon} alt="" />
+            <input
+              type={passwordType}
+              id="password"
+              placeholder="*********"
+              className="input-password"
+            />
+            <button
+              type="button"
+              className="btn-mata"
+              onClick={togglePasswordVisibility}
+            >
+              <img src={mataIcon} alt="" />
+            </button>
+          </div>
+          <p className="judul-form">Sandi baru</p>
+          <div className="con-form-password">
+            <img src={passIcon} alt="" />
+            <input
+              type={passwordTypeNew}
+              id="newPassword"
+              placeholder="*********"
+              className="input-password"
+            />
+            <button
+              type="button"
+              className="btn-mata"
+              onClick={togglePasswordVisibilityNew}
+            >
+              <img src={mataIcon} alt="" />
+            </button>
+          </div>
+          <p className="judul-form">Konfirmasi sandi baru</p>
+          <div className="con-form-password">
+            <img src={passIcon} alt="" />
+            <input
+              type={passwordTypeConfirm}
+              id="confirmPassword"
+              placeholder="*********"
+              className="input-password"
+            />
+            <button
+              type="button"
+              className="btn-mata"
+              onClick={togglePasswordVisibilityConfirm}
+            >
+              <img src={mataIcon} alt="" />
+            </button>
+          </div>
+
+          <button type="submit" className="btn-simpan">
+            Simpan sandi baru
+          </button>
+        </form>
+      </div>
+
+      {dataProfileOrtu.map((profileSiswa) => (
+        <div className="detail-profile">
+          <div className="content-detail">
+            <div className="navbar-detail">
+              <Icon
+                icon="radix-icons:cross-circled"
+                width="30"
+                style={{ cursor: "pointer" }}
+                onClick={closeDetail}
+              />
+              <h2>Profil</h2>
+            </div>
+            <div className="detail-image-profile">
+              <img
+                src={`https://www.nugasyuk.my.id/public/${profileSiswa.foto_profile}`}
+                alt=""
+                className="detail-img-profile"
+              />
+            </div>
+            <p className="judul-detail">Email</p>
+            <p className="value-detail">{profileSiswa.email}</p>
+            <p className="judul-detail">Nama Pengguna</p>
+            <p className="value-detail">{profileSiswa.nama_panggilan}</p>
+            <p className="judul-detail">Nama</p>
+            <p className="value-detail">{profileSiswa.nama_siswa}</p>
+            <p className="judul-detail">Jurusan</p>
+            <p className="value-detail">{profileSiswa.nama_jurusan}</p>
+            <p className="judul-detail">Kelas</p>
+            <p className="value-detail">{profileSiswa.tingkat_ke}</p>
+            <p className="judul-detail">NIS</p>
+            <p className="value-detail">{profileSiswa.nis}</p>
+          </div>
+          <div className="con-btn-detail-profile">
+            <button
+              className="forget-password"
+              id="btn-forget-pass"
+              onClick={showForgetPopup}
+            >
+              <Icon icon="material-symbols:key-outline-rounded" width="30" />
+              <p>Ganti Password</p>
+            </button>
+            <button
+              className="logout"
+              id="btn-logout"
+              onClick={showLogoutPopup}
+            >
+              <Icon icon="material-symbols:logout-rounded" width="30" />
+              <p>Logout</p>
+            </button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
 
-export default NavbarWaliMurid
+export default NavbarWaliMurid;
